b0cda22e68697b6a2859d15184370f8e8e412b1b
Arunn2287 <kerner9@gmail.com>
2020-11-16 22:46:55 +0100

    new timer

--- main.lua
+++ main.lua
@@ -40,38 +40,36 @@ frame:SetScript("OnClick", function()
 end)
 
 function Run()
-    local timeSpanHour = 6;
-    local timeSpanMin = 40;
+    local timeSpanHour = 3;
+    local timeSpanMin = 20;
     local baseDay = 12;
 
     local hour, minute = GetGameTime()
     local day = date("%d")
     local currentTimeInMin = (day*60*60) + (hour*60) + minute;
-
     local boss = 
-        {
-            [1]  = { ["name"] = "Skadi the Ruthless",           ["coorX"] = "57,8",     ["coorY"] = "56,1", ["day"] =  12, ["hour"] = 17, ["min"] = 40},
-            [2]  = { ["name"] = "Ingvar the Plunderer",         ["coorX"] = "52,4",     ["coorY"] = "52,6", ["day"] =  12, ["hour"] = 18, ["min"] = 0},
-            [3]  = { ["name"] = "Prince Keleseth",              ["coorX"] = "54,0",     ["coorY"] = "44,7", ["day"] =  12, ["hour"] = 18, ["min"] = 20},
-            [4]  = { ["name"] = "The Black Knight",             ["coorX"] = "64,8",     ["coorY"] = "22,1", ["day"] =  12, ["hour"] = 18, ["min"] = 40},
-            [5]  = { ["name"] = "Bronjahm",                     ["coorX"] = "70,7",     ["coorY"] = "38,4", ["day"] =  12, ["hour"] = 19, ["min"] = 0},
-            [6]  = { ["name"] = "Scourgelord Tyrannus",         ["coorX"] = "47,2",     ["coorY"] = "66,1", ["day"] =  12, ["hour"] = 19, ["min"] = 20},
-            [7]  = { ["name"] = "Forgemaster Garfrost",         ["coorX"] = "58,6",     ["coorY"] = "72,5", ["day"] =  12, ["hour"] = 19, ["min"] = 40},
-            [8]  = { ["name"] = "Marwyn",                       ["coorX"] = "58,2",     ["coorY"] = "83,4", ["day"] =  12, ["hour"] = 20, ["min"] = 0},
-            [9]  = { ["name"] = "Falric",                       ["coorX"] = "50,2",     ["coorY"] = "87,9", ["day"] =  12, ["hour"] = 20, ["min"] = 20},
-            [10] = { ["name"] = "The Prophet Tharon'ja",        ["coorX"] = "80,1",     ["coorY"] = "61,2", ["day"] =  12, ["hour"] = 20, ["min"] = 40},
-            [11] = { ["name"] = "Novos the Summoner",           ["coorX"] = "77,8",     ["coorY"] = "66,1", ["day"] =  12, ["hour"] = 21, ["min"] = 0},
-            [12] = { ["name"] = "Trollgore",                    ["coorX"] = "58,3",     ["coorY"] = "39,4", ["day"] =  12, ["hour"] = 21, ["min"] = 20},
-            [13] = { ["name"] = "Krik'thir the Gatewatcher",    ["coorX"] = "67,5",     ["coorY"] = "58,0", ["day"] =  12, ["hour"] = 21, ["min"] = 40},
-            [14] = { ["name"] = "Prince Taldaram",              ["coorX"] = "29,6",     ["coorY"] = "62,2", ["day"] =  12, ["hour"] = 22, ["min"] = 0},
-            [15] = { ["name"] = "Elder Nadox",                  ["coorX"] = "44,2",     ["coorY"] = "49,1", ["day"] =  12, ["hour"] = 22, ["min"] = 20},
-            [16] = { ["name"] = "Noth the Plaguebringer",       ["coorX"] = "31,6",     ["coorY"] = "70,5", ["day"] =  12, ["hour"] = 22, ["min"] = 40},
-            [17] = { ["name"] = "Patchwerk (in cave)",          ["coorX"] = "34,4",     ["coorY"] = "68,3", ["day"] =  12, ["hour"] = 23, ["min"] = 0},
-            [18] = { ["name"] = "Blood Queen Lana'thel",        ["coorX"] = "49,7",     ["coorY"] = "32,7", ["day"] =  12, ["hour"] = 23, ["min"] = 20},
-            [19] = { ["name"] = "Professor Putricide",          ["coorX"] = "57,1",     ["coorY"] = "30,3", ["day"] =  12, ["hour"] = 23, ["min"] = 40},
-            [20] = { ["name"] = "Lady Deathwhisper",            ["coorX"] = "51,1",     ["coorY"] = "78,5", ["day"] =  13, ["hour"] = 0,  ["min"] = 0}
-        };
-
+    {
+        [1]  = { ["name"] = "Skadi the Ruthless",           ["coorX"] = "57,8",     ["coorY"] = "56,1", ["day"] =  16, ["hour"] = 17, ["min"] = 30},
+        [2]  = { ["name"] = "Ingvar the Plunderer",         ["coorX"] = "52,4",     ["coorY"] = "52,6", ["day"] =  16, ["hour"] = 17, ["min"] = 40},
+        [3]  = { ["name"] = "Prince Keleseth",              ["coorX"] = "54,0",     ["coorY"] = "44,7", ["day"] =  16, ["hour"] = 17, ["min"] = 50},
+        [4]  = { ["name"] = "The Black Knight",             ["coorX"] = "64,8",     ["coorY"] = "22,1", ["day"] =  16, ["hour"] = 18, ["min"] = 0},
+        [5]  = { ["name"] = "Bronjahm",                     ["coorX"] = "70,7",     ["coorY"] = "38,4", ["day"] =  16, ["hour"] = 18, ["min"] = 10},
+        [6]  = { ["name"] = "Scourgelord Tyrannus",         ["coorX"] = "47,2",     ["coorY"] = "66,1", ["day"] =  16, ["hour"] = 18, ["min"] = 20},
+        [7]  = { ["name"] = "Forgemaster Garfrost",         ["coorX"] = "58,6",     ["coorY"] = "72,5", ["day"] =  16, ["hour"] = 18, ["min"] = 30},
+        [8]  = { ["name"] = "Marwyn",                       ["coorX"] = "58,2",     ["coorY"] = "83,4", ["day"] =  16, ["hour"] = 18, ["min"] = 40},
+        [9]  = { ["name"] = "Falric",                       ["coorX"] = "50,2",     ["coorY"] = "87,9", ["day"] =  16, ["hour"] = 18, ["min"] = 50},
+        [10] = { ["name"] = "The Prophet Tharon'ja",        ["coorX"] = "80,1",     ["coorY"] = "61,2", ["day"] =  16, ["hour"] = 19, ["min"] = 0},
+        [11] = { ["name"] = "Novos the Summoner",           ["coorX"] = "77,8",     ["coorY"] = "66,1", ["day"] =  16, ["hour"] = 19, ["min"] = 10},
+        [12] = { ["name"] = "Trollgore",                    ["coorX"] = "58,3",     ["coorY"] = "39,4", ["day"] =  16, ["hour"] = 19, ["min"] = 20},
+        [13] = { ["name"] = "Krik'thir the Gatewatcher",    ["coorX"] = "67,5",     ["coorY"] = "58,0", ["day"] =  16, ["hour"] = 19, ["min"] = 30},
+        [14] = { ["name"] = "Prince Taldaram",              ["coorX"] = "29,6",     ["coorY"] = "62,2", ["day"] =  16, ["hour"] = 19, ["min"] = 40},
+        [15] = { ["name"] = "Elder Nadox",                  ["coorX"] = "44,2",     ["coorY"] = "49,1", ["day"] =  16, ["hour"] = 19, ["min"] = 50},
+        [16] = { ["name"] = "Noth the Plaguebringer",       ["coorX"] = "31,6",     ["coorY"] = "70,5", ["day"] =  16, ["hour"] = 20, ["min"] = 0},
+        [17] = { ["name"] = "Patchwerk (in cave)",          ["coorX"] = "34,4",     ["coorY"] = "68,3", ["day"] =  16, ["hour"] = 17, ["min"] = 10},
+        [18] = { ["name"] = "Blood Queen Lana'thel",        ["coorX"] = "49,7",     ["coorY"] = "32,7", ["day"] =  16, ["hour"] = 17, ["min"] = 0},
+        [19] = { ["name"] = "Professor Putricide",          ["coorX"] = "57,1",     ["coorY"] = "30,3", ["day"] =  16, ["hour"] = 17, ["min"] = 10},
+        [20] = { ["name"] = "Lady Deathwhisper",            ["coorX"] = "51,1",     ["coorY"] = "78,5", ["day"] =  16, ["hour"] = 17,  ["min"] = 20}
+    };
 
     for id, item in pairs(boss) do
         local bossDay = item.day;
